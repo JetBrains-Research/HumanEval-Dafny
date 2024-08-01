f5f9a53b283faeb0ea8b2f8dfe06be00614dde61
WeetHet <stas.ale66@gmail.com>
2024-08-01 18:09:36 +0400

    Use nix run

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Test dafny projects";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = pkgs.writeShellScriptBin "dafny-test-all" ''
+            for f in *.dfy
+            do
+                echo "Testing $f"
+                ${pkgs.dafny}/bin/dafny verify --verification-time-limit 10 $f
+            done
+        '';
+      };
+    };
+}
--- scripts/dafny-test-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script runs all dafny tests in the current directory.
-
-for f in *.dfy
-do
-    echo "Testing $f"
-    dafny verify --verification-time-limit 10 $f
-done

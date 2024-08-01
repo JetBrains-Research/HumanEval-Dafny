cc0b1341cc8dd15519fc70148465800b14687661
WeetHet <stas.ale66@gmail.com>
2024-08-01 18:33:22 +0400

    Remove unused arguments from perSystem

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { pkgs, ... }: {
         packages.default = pkgs.writeShellScriptBin "dafny-test-all" ''
             for f in *.dfy
             do
